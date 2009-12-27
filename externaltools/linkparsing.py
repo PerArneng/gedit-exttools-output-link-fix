7c72ee3723826722d9ba928a3ea8e6d97610991e
Per Arneng <per.arneng@anyplanet.com>
2009-12-27 23:33:45 +0100

    added some regex for gcc output

--- externaltools/linkparsing.py
+++ externaltools/linkparsing.py
@@ -1,6 +1,8 @@
 
+import re
+
 class Link:
-    
+
     def __init__(self, path, line_nr):
         self._path = path
         self._line_nr = line_nr
@@ -22,15 +24,20 @@ class LinkParser:
 
         return link
 
-
 class LinkParserProvider:
 
     def parse(self, line):
         raise NotImplementedError("need to implement a parse method")
 
 class GccLinkParserProvider(LinkParserProvider):
-    
+
+    def __init__(self):
+        self.fm = re.compile("^(.*)\:(\d+)\:", re.MULTILINE)
+
     def parse(self, line):
+        for m in re.finditer(self.fm, line):
+            print "%s %s" % (m.group(1), m.group(2))
+        
         print line
         return None
 
--- externaltools/linkparsing_test.py
+++ externaltools/linkparsing_test.py
@@ -8,7 +8,17 @@ class LinkParserTest(unittest.TestCase):
         self.p = LinkParser()
 
     def test_parse_gcc_simple(self):
-        self.p.parse("test.c:12: error: sometext")
+        gcc_output = """
+test.c: In function 'f':
+test.c:5: warning: passing argument 1 of 'f' makes integer from pointer without a cast
+test.c:3: note: expected 'int' but argument is of type 'char *'
+test.c: In function 'main':
+test.c:11: warning: initialization makes pointer from integer without a cast
+test.c:12: warning: initialization makes integer from pointer without a cast
+test.c:13: error: too few arguments to function 'f'
+test.c:14: error: expected ';' before 'return'
+"""
+        self.p.parse(gcc_output)
 
 if __name__ == '__main__':
     unittest.main()
